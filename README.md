524c487cb8c855b571ee1959c48bd6519bd96067
ivandjoh <ivandi.djoh@gmail.com>
2023-01-31 03:22:22 +0800

    Major Change

---
+++ README.md
@@ -0,0 +1,5 @@
+## AccelByte-CLI
+
+### About AccelByte CLI
+AccelByte CLI is a command line interface for AccelByte platform.
+It is a simple tool to view directory and files inside it.
---
+++ src/main/java/com/ivandjoh/App.java
@@ -0,0 +1,32 @@
+package com.ivandjoh;
+
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(name = "accelByte", description = "AccelByte CLI", mixinStandardHelpOptions = true, version = "1.0")
+public class App implements Callable<String> {
+
+    @CommandLine.Option(names = "-s", description = "Source folder")
+    private String sourceFolder;
+
+    @CommandLine.Option(names = "-d", description = "Destination folder")
+    private String destinationFolder;
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public String call() throws Exception {
+        if (sourceFolder == null || destinationFolder == null) {
+            System.out.println("Source and destination folder must be specified");
+            System.exit(1);
+        }
+        System.out.println("Source folder: " + sourceFolder);
+        System.out.println("Destination folder: " + destinationFolder);
+        return "Success";
+    }
+}
